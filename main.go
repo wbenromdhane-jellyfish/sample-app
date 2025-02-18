3d71a37c0090405ba82b313a65ead51f09e9d354
wbenromdhane-jellyfish <student-04-3866721dd78e@qwiklabs.net>
2025-02-18 01:50:11 +0000

    Update dev deployment to version v2.0 with redHandler

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
-package main() {
+func main() {
     http.HandleFunc("/blue", blueHandler)
     http.HandleFunc("/red", redHandler)
     http.ListenAndServe(":8080", nil)
 }
 
-package redHandler(w http.ResponseWriter, r *http.Request) {
+func redHandler(w http.ResponseWriter, r *http.Request) {
     img := image.NewRGBA(image.Rect(0, 0, 100, 100))
     draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
     w.Header().Set("Content-Type", "image/png")
